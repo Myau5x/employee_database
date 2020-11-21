0b3a65cc7df85917abf34ed1d5665429e35e3707
Olga Illarionova <ola.illari@gmail.com>
2020-11-20 20:21:50 -0800

    create db

---
+++ seed.sql
@@ -0,0 +1,20 @@
+USE employeeDB;
+
+INSERT INTO departments (id, name)
+VALUES (1,"IT");
+
+INSERT INTO departments (id, name)
+VALUES (2,"Sales");
+
+INSERT INTO roles (id, title,salary, department_id)
+VALUES (1,"Support",50000.00, 1);
+
+INSERT INTO roles (id, title,salary, department_id)
+VALUES (2,"IT Director",250000.00, 1);
+
+INSERT INTO roles (id, title,salary, department_id)
+VALUES (2,"Salesman",50000.00, 1);
+
+INSERT INTO employees ( first_name,last_name, role_id)
+VALUES ('Olga','Illarionova',2);
+
